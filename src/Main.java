8ff9f6d8d39415f9e0926d050ede36a8f2c20b85
Nikita <nik.nogvi@gmail.com>
2023-04-20 00:37:43 +0300

    add min func

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,30 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         int b = 15;
-        System.out.println(sum(a,b));
-        System.out.println(minus(a,b));
-        System.out.println(multiplay(a,b));
-        System.out.println(devide(a,b));
+        System.out.println(sum(a, b));
+        System.out.println(minus(a, b));
+        System.out.println(multiplay(a, b));
+        System.out.println(devide(a, b));
+        System.out.println(min(b, a));
     }
 
-    private static int sum (int a, int b) {
+    private static int sum(int a, int b) {
         return a + b;
     }
 
-    private static int minus (int a, int b) {
+    private static int minus(int a, int b) {
         return a - b;
     }
 
-    private static int multiplay (int a, int b) {
+    private static int multiplay(int a, int b) {
         return a * b;
     }
 
-    private static int devide (int a, int b) {
+    private static int devide(int a, int b) {
         return a / b;
     }
 
+    private static int min(int a, int b) {
+        return a < b ? a : b;
+    }
 }
