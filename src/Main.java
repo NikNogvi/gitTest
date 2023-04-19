25a2367dec197722ec632c9e3184ff2316d7fe37
Nikita <nik.nogvi@gmail.com>
2023-04-20 00:20:18 +0300

    add sum function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 10;
+        int b = 15;
+        System.out.println(sum(a,b));
+    }
+
+    private static int sum (int a, int b) {
+        return a + b;
     }
 }
