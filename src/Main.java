f1f3879fd37aa3cb4f3bc8a3e1bd50827b23d0e9
Nikita <nik.nogvi@gmail.com>
2023-04-20 00:23:43 +0300

    add devide function

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println(sum(a,b));
         System.out.println(minus(a,b));
         System.out.println(multiplay(a,b));
+        System.out.println(devide(a,b));
     }
 
     private static int sum (int a, int b) {
@@ -17,6 +18,10 @@ public class Main {
 
     private static int multiplay (int a, int b) {
         return a * b;
+    }
+
+    private static int devide (int a, int b) {
+        return a / b;
     }
 
 }
